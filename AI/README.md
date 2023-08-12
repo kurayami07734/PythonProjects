de2f61337633be7521964c07cd89789bdadbed58
Aditya Ghidora <aditya@ghidora.tech>
2023-08-12 22:50:35 +0530

    bfs dfs maze solution

---
+++ AI/README.md
@@ -0,0 +1,15 @@
+# Notes
+
+<details>
+<summary>
+Basic Terms
+</summary>
+
+1. Agent - An entity which is able to percieve and act on its environment
+2. State - Any configuration of state and the agent.
+3. Actions - A valid change in state that can be made by the agent. (Typically denoted as a function of state)
+4. Transition model - Defines the way the state changes after action is performed. (Denoted as a results function of state and action)
+5. State space - Set of possible states after applying any valid sequence of actions.
+6. Goal test - a method to check if goal state is achieved.
+7. Path cost - Cost associated with the path taken to reach goal state. Usually used to optimize the path.
+</details>
--- Selenium/results_bot.py
+++ Selenium/results_bot.py
@@ -8,17 +8,18 @@ from concurrent.futures import ThreadPoolExecutor
 from tabula import read_pdf
 from pandas import DataFrame
 
-rollNumbers = [i for i in range(20117001, 20117130)]
-sliders = [i for i in range(20117901, 20117920)]
+rollNumbers = [i for i in range(21117001, 21117130)]
+sliders = [i for i in range(21117901, 21117920)]
 rollNumbers.extend(sliders)
 resultsData = {}
 
 
 def find_file_url(rollNumber: int) -> tuple[str, str]:
     opts = Options()
-    opts.headless = True
+    opts.add_argument("-headless")
     browser = webdriver.Firefox(options=opts)
     browser.set_window_size(1440, 900)
+
     wait = WebDriverWait(browser, 8)
     browser.get("https://mis.nitrr.ac.in/publishedresult.aspx")
 
@@ -32,12 +33,12 @@ def find_file_url(rollNumber: int) -> tuple[str, str]:
 
         sessionMenu = wait.until(
             EC.presence_of_element_located((By.ID, "ddlSession")))
-        Select(sessionMenu).select_by_value('102')
+        Select(sessionMenu).select_by_value('104')
 
         semMenu = wait.until(
             EC.presence_of_element_located((By.ID, "ddlSemester")))
 
-        Select(semMenu).select_by_value('5')
+        Select(semMenu).select_by_value('4')
 
         main_window = browser.current_window_handle
 
@@ -49,6 +50,7 @@ def find_file_url(rollNumber: int) -> tuple[str, str]:
                 wait.until(EC.url_changes("about:blank"))
                 file_url = browser.current_url
                 browser.quit()
+                print(f'{name} : {file_url} found')
                 return (name, file_url)
 
     except Exception as e:
@@ -61,22 +63,43 @@ def scrapePDF(nameAndURL: tuple[str, str], rollNumber: int):
         resultsData[rollNumber] = {
             "Name": nameAndURL[0], "Error": nameAndURL[1][:15]}
         return
-    df = read_pdf(nameAndURL[1],
-                  area=(130, 47, 260, 565), pages=1)[0]
-    subjects = df['Name of Subjects'].to_list()
-    grades = df["Grade Point"].to_list()
-    d = zip(subjects, grades)
-    ptrs = [float(ptr.split(":")[1]) for ptr in read_pdf(
-        nameAndURL[1], area=(250, 100, 268, 450), pages=1)[0].columns.values.tolist()]
-    if ptrs[0] > 10 or ptrs[1] > 10:
-        data = {'SPI': 'F', 'CPI': 'F', "Name": nameAndURL[0], "Error": "None"}
-    else:
-        data = {'SPI': ptrs[0], 'CPI': ptrs[1],
-                "Name": nameAndURL[0], "Error": "None"}
-    for sub, grd in d:
-        data[sub] = grd
-    resultsData[rollNumber] = data
-    print(f'{len(resultsData)} {rollNumber} : {resultsData[rollNumber]}')
+    try:
+        df = read_pdf(nameAndURL[1],
+                      area=(130, 47, 220, 565), pages=1)[0]
+        subjects = df['Name of Subjects'].to_list()
+        grades = df["Grade Point"].to_list()
+        d = zip(subjects, grades)
+        pointerArea = read_pdf(
+            nameAndURL[1], area=(237, 100, 260, 450), pages=1)[0].columns.values.tolist()
+        if "Offer Credit" not in pointerArea[0]:
+            ptrs = [ptr.split(":")[1]
+                    for ptr in pointerArea]
+            if isNumber(ptrs[0]) and isNumber(ptrs[1]):
+                ptrs = [float(ptrs[0]), float(ptrs[1])]
+            if ptrs[0] > 10 or ptrs[1] > 10:
+                data = {'SPI': 'F', 'CPI': 'F',
+                        "Name": nameAndURL[0], "URL": nameAndURL[1], "Error": "None"}
+            else:
+                data = {'SPI': ptrs[0], 'CPI': ptrs[1],
+                        "Name": nameAndURL[0], "URL": nameAndURL[1], "Error": "None"}
+        else:
+            data = {'SPI': 'F', 'CPI': 'F',
+                    "Name": nameAndURL[0], "URL": nameAndURL[1], "Error": "None"}
+        for sub, grd in d:
+            data[sub] = grd
+        resultsData[rollNumber] = data
+        print(f'{len(resultsData)} {rollNumber} : {resultsData[rollNumber]}')
+
+    except Exception as e:
+        print(f"{rollNumber} : {str(e)}")
+
+
+def isNumber(num: str) -> bool:
+    try:
+        float(num)
+    except ValueError:
+        return False
+    return True
 
 
 def fetchResults(rollNumber: int):
@@ -84,12 +107,12 @@ def fetchResults(rollNumber: int):
 
 
 def main():
-    # fetchResults(rollNumber=20115050)
     with ThreadPoolExecutor() as executor:
         executor.map(fetchResults, rollNumbers)
-
+    # fetchResults(20117011)
+    # scrapePDF(("CHIRAG GAJBHIYE", "https://mis.nitrr.ac.in/Reports/commonreportForCBCS.aspx?pagetitle=Tabulation_Sheet&path=~,Reports,Academic,rptTabulationRegistarCBCSStud_PubResult.rpt&param=@P_SEMESTER=6,@P_IDNO=16466,@P_SESSIONNO=104,@P_COLLEGE_CODE=53"), 20117023)
     df = DataFrame(resultsData).transpose()
-    df.to_csv('results_EE.csv')
+    df.to_csv('results_EE_IV_JUNIORS.csv')
     print(df)
 
 
